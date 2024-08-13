a1b77b9bd42df710e5d9c2e6b6f3f508ba6d2cb0
ko-ki-creator <c9xj.c9ajmf2@gmail.com>
2024-08-13 17:09:12 +0900

    event.js

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,10 +1,11 @@
-$(function() {
   $(window).on('load', () => {
     // ドキュメントが読み込み完了した時
     console.log('loadイベントが発生しました');
     });
+
+    $(function() {
     // 画面をスクロールした時
-    $(window).on('scroll', () => {
+    $(window).on('scroll', function() {
     console.log('scrollイベントが発生しました');
   });
 });
